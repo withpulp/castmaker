e726bfdcd7c1ce1a160cf30a96941a9f78357c50
Damir Vazgird <damir@withpulp.com>
2017-09-03 00:17:52 -0400

    refactor quote and seo

---
+++ src/components/quote/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './index.css';
+
+class Quote extends React.Component {
+  renderQuote() {
+    const { data } = this.props;
+
+    if (data) {
+      return (
+        <blockquote className="quote">
+          {`"${data.quote}"`}
+          <span className="author">- {data.author}</span>
+        </blockquote>
+      )
+    } else {
+      return null;
+    }
+  }
+
+  render() {
+    const { type, figure, data } = this.props;
+    let quote;
+
+    if (figure) {
+      quote = (
+        <figure className={type ? `${type} quote figure` : `quote figure`}>
+          { this.renderQuote() }
+        </figure>
+      );
+    } else {
+      quote = (
+        <blockquote className={type ? `${type} quote` : `quote`}>
+          {`"${data.quote}"`}
+          <span className="author">- {data.author}</span>
+        </blockquote>
+      );
+    }
+
+    return quote;
+  }
+}
+
+export default Quote;
--- src/containers/footer/index.jsx
+++ src/containers/footer/index.jsx
@@ -1,21 +1,25 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import Link from 'gatsby-link';
-import Quotes from '../../components/quotes/';
+import Quote from '../../components/quote/';
 import config from '../../../data/config';
 import quotes from '../../../data/quotes';
 import './index.css';
 
 class Footer extends Component {
+  getQuote() {
+    return _.sample(quotes);
+  }
+
   render() {
-    const url = config.siteRss;
+    const quote = this.getQuote();
     // @TODO: create caption component for copyright message
     // add url to company/author website inside copyright message
-    
+
     return (
       <footer className="footer">
         <section className="affixed bottom section">
-          <Quotes data={quotes} />
+          <Quote figure data={quote} />
           <p className="copyright caption">{config.copyright}</p>
         </section>
       </footer>
